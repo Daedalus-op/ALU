257bcefd94e1bb74306807cb2616349fc3f7f1de
ubundadu <varkeyprana@gmail.com>
2024-06-03 09:31:09 +0530

    1st failed attempt at multiplier @ .mul_clueless

--- .mul_clueless.v
+++ .mul_clueless.v
@@ -1,15 +1,15 @@
-module mul(a, b, t1, t2);
+module mul(a, b, pro);
 input [1:0] a;
 input [1:0] b;
-//output [3:0] pro;
+output [3:0] pro;
 
-output reg [2:0] t1;
-output reg [2:0] t2;
+wire reg [2:0] p1;
+wire reg [2:0] p2;
 always @* begin
-  t1 = b;
-  t2 = b << 1;
+  p1 = a & {b[0],b[0]};
+  p2 = (a & {b[1],b[1]}) << 1;
 end
-//rca_add #(3) unit(t2 , t1, pro);
+rca_add #(3) unit(t2 , t1, pro);
 
 //assign c = (t2 << 1) + t1;
 
