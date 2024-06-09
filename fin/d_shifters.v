bc2c0e72ed16eb5a03d9b0e9d1898258aa76b4de
ubundadu <varkeyprana@gmail.com>
2024-06-09 23:03:43 +0530

    Made shifting synchronous with clk

--- fin/d_shifters.v
+++ fin/d_shifters.v
@@ -1,37 +1,32 @@
-module shifter #(parameter N=4)(
+module shift #(parameter N=4)(
   input dir,
   input clk,
-  input rst,
+  input en,
   input [N - 1:0] a,
   output reg [N - 1:0] y
 );
 reg [N-1:0] i;
-reg en;
-always @(posedge clk , rst) begin
-  if (rst == 1'b1) begin
-      y <= 'd0;
-      en <= 1'b0;
-    end
-  else begin
-    if (en == 1'b0) begin
-      y <= a;
-      en <= 1'b1;
-    end
-    else begin
-      // Right Shifti
-      if ((dir == 1) & (clk == 1)) begin
-          for (i = 0; i < N - 1; i = i + 1) begin
-            y[i] <= y[i + 1];
-          end
-          y[N - 1] <= 1'b0;
+always @(en) begin
+  if (en == 1'b1) begin
+    y <= a;
+  end
+end
+
+always @(posedge clk) begin
+  if (en == 1'b0) begin
+    // Right Shift
+    if ((dir == 1) & (clk == 1)) begin
+      for (i = 0; i < N - 1; i = i + 1) begin
+        y[i] <= y[i + 1];
       end
-      // Left Shift
-      else if ((dir == 0) & (clk == 1)) begin
-          for (i = N - 1; i > 0; i = i - 1) begin
-            y[i] <= y[i - 1];
-          end
-          y[1] <= 1'b0;
+      y[N - 1] <= 1'b0;
+    end
+    // Left Shift
+    else if ((dir == 0) & (clk == 1)) begin
+      for (i = N - 1; i > 0; i = i - 1) begin
+        y[i] <= y[i - 1];
       end
+      y[1] <= 1'b0;
     end
   end
 end
--- fin/shifters_tb.v
+++ fin/shifters_tb.v
@@ -1,38 +1,46 @@
 module shift_tb;
 
-reg dir, clk, rst;
+reg dir, clk, en;
 reg [7:0] a;
 wire [7:0] y;
 
-shifter #(8) uut (dir, clk, rst, a, y);
+shift #(8) uut (dir, clk, en, a, y);
+reg [4:0] i;
 always begin
   //setup
   $dumpfile("play.vcd");
   $dumpvars(0, shift_tb);
 
-  clk = 1'b0; rst = 1'b1;
+  clk = 1'b0; en = 1'b0;
   #5
 
   //testing
-  dir = 1'b0; a = 8'b10101010; rst = 1'b0;
-  $display("%b%b%b %b", clk, dir, rst, y);
+  $display("\nDefault case\n");
+  dir = 1'b0; a = 8'b10101010; en = 1'b1;
+  $display("%b%b%b %b", clk, dir, en, y);
   #4
-  $display("%b%b%b %b", clk, dir, rst, y);
+  en = 1'b0;
+  $display("\nSample output :\n");
+  $display("%b%b%b %b", clk, dir, en, y);
   #4
-  $display("%b%b%b %b", clk, dir, rst, y);
+  $display("%b%b%b %b", clk, dir, en, y);
   #4
-  $display("%b%b%b %b", clk, dir, rst, y);
+  $display("%b%b%b %b", clk, dir, en, y);
   #4
-  rst = 1'b1;
+  $display("%b%b%b %b", clk, dir, en, y);
   #4
-  dir = 1'b1; a = 8'b11001101; rst = 1'b0;
-  $display("%b%b%b %b", clk, dir, rst, y);
+  $display("\nSample output :\n");
+  dir = 1'b1; a = 8'b11001101; en = 1'b1;
+  $display("%b%b%b %b", clk, dir, en, y);
   #4
-  $display("%b%b%b %b", clk, dir, rst, y);
+  en = 1'b0;
+  $display("%b%b%b %b", clk, dir, en, y);
   #4
-  $display("%b%b%b %b", clk, dir, rst, y);
+  $display("%b%b%b %b", clk, dir, en, y);
   #4
-  $display("%b%b%b %b", clk, dir, rst, y);
+  $display("%b%b%b %b", clk, dir, en, y);
+  #4
+  $display("%b%b%b %b", clk, dir, en, y);
   #20
   $finish;
 end
